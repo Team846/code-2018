936c50eb55db74b93e11460c69598c4c83cfcc45
Instynkt <alvyn.wang@gmail.com>
2018-01-30 20:12:54 -0800

    Delete unfinished class

--- src/main/scala/LaunchRobot.scala
+++
@@ -1,9 +0,0 @@
-import edu.wpi.first.wpilibj.{RobotBase}
-
-class LaunchRobot extends RobotBase {
-  override def teleopInit(): Unit = println("Hello world!")
-
-  override def teleopPeriodic(): Unit = {
-    // TODO code robot
-  }
-}
