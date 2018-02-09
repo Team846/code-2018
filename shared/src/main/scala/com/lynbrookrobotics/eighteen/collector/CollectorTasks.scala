d5124dab991ec3a9bd7b598e0d7ae7d105db4c89
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-02-08 20:50:40 -0800

    Fix compilation errors

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/collector/CollectorTasks.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/collector/CollectorTasks.scala
@@ -1,15 +1,18 @@
 package com.lynbrookrobotics.eighteen.collector
 
 import com.lynbrookrobotics.eighteen.collector.clamp.{ClampCollector, CollectorClamp}
-import com.lynbrookrobotics.eighteen.collector.rollers.{CollectorRollers, SpinForCollect, SpinForPurge}
+import com.lynbrookrobotics.eighteen.collector.rollers.{CollectorRollers, CollectorRollersProperties, SpinForCollect, SpinForPurge}
+import com.lynbrookrobotics.potassium.Signal
 import com.lynbrookrobotics.potassium.tasks.ContinuousTask
 
 object CollectorTasks {
-  def collectCubeClamped(rollers: CollectorRollers, clamp: CollectorClamp): ContinuousTask = {
+  def collectCubeClamped(rollers: CollectorRollers, clamp: CollectorClamp)
+                        (implicit collectorRollersProps: Signal[CollectorRollersProperties]): ContinuousTask = {
     new SpinForCollect(rollers) and new ClampCollector(clamp)
   }
 
-  def purgeCubeClamped(rollers: CollectorRollers, clamp: CollectorClamp): ContinuousTask = {
+  def purgeCubeClamped(rollers: CollectorRollers, clamp: CollectorClamp)
+                      (implicit collectorRollersProps: Signal[CollectorRollersProperties]): ContinuousTask = {
     new SpinForPurge(rollers) and new ClampCollector(clamp)
   }
 }
