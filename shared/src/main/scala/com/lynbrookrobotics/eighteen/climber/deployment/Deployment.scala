779b40b0077498ac39e916a6ece813de52a019f8
Alvyn Wang <alvyn.wang@gmail.com>
2018-02-12 19:41:13 -0800

    Implement climber deployment

---
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/deployment/Deployment.scala
@@ -0,0 +1,26 @@
+package com.lynbrookrobotics.eighteen.climber.deployment
+
+import com.lynbrookrobotics.potassium.Component
+import com.lynbrookrobotics.potassium.streams.Stream
+
+trait DeploymentState
+
+case object DeploymentOn extends DeploymentState
+case object DeploymentOff extends DeploymentState
+
+class Deployment(coreTicks: Stream[Unit]) (implicit hardware: DeploymentHardware) extends Component[DeploymentState] {
+  override def defaultController: Stream[DeploymentState] = coreTicks.mapToConstant(DeploymentOff)
+
+  override def applySignal(signal: DeploymentState): Unit = {
+    signal match {
+      case DeploymentOn => {
+        hardware.solenoidLeft.set(true)
+        hardware.solenoidRight.set(true)
+      }
+      case DeploymentOff => {
+        hardware.solenoidLeft.set(false)
+        hardware.solenoidRight.set(false)
+      }
+    }
+  }
+}
