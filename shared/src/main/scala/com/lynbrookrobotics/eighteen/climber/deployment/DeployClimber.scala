09856859a1d9798822eba6727116e377cdc9635f
Alvyn Wang <alvyn.wang@gmail.com>
2018-02-13 19:46:39 -0800

    Create task to deploy climber

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/deployment/DeployClimber.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/deployment/DeployClimber.scala
@@ -1,5 +1,9 @@
 package com.lynbrookrobotics.eighteen.climber.deployment
 
-class DeployClimber {
+import com.lynbrookrobotics.potassium.tasks.ContinuousTask
 
+class DeployClimber(deployment: Deployment) extends ContinuousTask{
+  override protected def onStart(): Unit = deployment.setController(deployment.coreTicks.mapToConstant(DeploymentOn))
+
+  override protected def onEnd(): Unit = deployment.resetToDefault()
 }
