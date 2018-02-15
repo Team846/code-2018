53b50a374d82a3ff2db6267bafd032c528653367
Instynkt <alvyn.wang@gmail.com>
2018-02-14 20:49:54 -0800

    Implement climber deployment (#10)
    * Implement climber deployment
    
    * Create DeployClimber Task
    
    * Create task to deploy climber
    
    * Fix DeployClimb.scala formatting

---
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/deployment/DeploymentHardware.scala
@@ -0,0 +1,14 @@
+package com.lynbrookrobotics.eighteen.climber.deployment
+
+import edu.wpi.first.wpilibj.Solenoid
+
+case class DeploymentHardware(solenoidLeft: Solenoid, solenoidRight: Solenoid)
+
+object DeploymentHardware {
+  def apply(config: DeploymentConfig): DeploymentHardware = {
+    DeploymentHardware(
+      new Solenoid(config.solenoidLeftPort),
+      new Solenoid(config.solenoidRightPort)
+    )
+  }
+}
