d257f712b9f1636f4bc9bba5249227ceff13908a
Kunal Sheth <kunal@kunalsheth.info>
2018-02-16 03:28:52 -0800

    implemented toNative and fromNative

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftProperties.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftProperties.scala
@@ -1,13 +1,13 @@
 package com.lynbrookrobotics.eighteen.lift
 
-import com.lynbrookrobotics.potassium.commons.lift.LiftProperties
 import com.lynbrookrobotics.potassium.commons.lift.offloaded.OffloadedLiftProperties
 import com.lynbrookrobotics.potassium.control.PIDConfig
 import com.lynbrookrobotics.potassium.control.offload.EscConfig
-import com.lynbrookrobotics.potassium.units.{GenericIntegral, GenericValue}
+import com.lynbrookrobotics.potassium.units.{GenericIntegral, GenericValue, _}
 import squants.Dimensionless
+import squants.electro.ElectricPotential
 import squants.motion.Velocity
-import squants.space.{Inches, Length}
+import squants.space.Length
 
 case class CubeLiftProperties(
                                pidConfig: PIDConfig[Length,
@@ -15,13 +15,20 @@ case class CubeLiftProperties(
                                  GenericValue[Length],
                                  Velocity,
                                  GenericIntegral[Length],
-                                 Dimensionless]
+                                 Dimensionless],
+                               potentiometerConversion: Ratio[ElectricPotential, Length],
+                               adc: Ratio[Dimensionless, ElectricPotential],
+                               bottom: ElectricPotential
                              ) extends OffloadedLiftProperties() {
   override def positionGains: PIDConfig[Length, Length, GenericValue[Length], Velocity, GenericIntegral[Length], Dimensionless] = pidConfig
 
-  override val escConfig: EscConfig[Length] = ???
+  override val escConfig: EscConfig[Length] = EscConfig(
+    ticksPerUnit = adc * potentiometerConversion
+  )
 
-  override def toNative(height: Length): Dimensionless = ???
+  override def toNative(height: Length): Dimensionless =
+    adc * (potentiometerConversion * height + bottom)
 
-  override def fromNative(native: Dimensionless): Length = ???
+  override def fromNative(native: Dimensionless): Length =
+    potentiometerConversion.recip * (adc.recip * native - bottom)
 }
