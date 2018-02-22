c445c229c80e53f979772d182d0151d33a2e35fa
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-02-22 10:34:39 -0800

    Run scalafmt

--- jvm/src/test/scala/com/lynbrookrobotics/eighteen/ConfigGenerator.scala
+++ jvm/src/test/scala/com/lynbrookrobotics/eighteen/ConfigGenerator.scala
@@ -22,75 +22,81 @@ object ConfigGenerator extends App {
       collectorClamp = None,
       collectorPivot = None,
       collectorRollers = None,
-      driver = Some(DriverConfig(
-        driverPort = 0,
-        operatorPort = 1,
-        driverWheelPort = 2,
-        launchpadPort = -1
-      )),
-      drivetrain = Some(DrivetrainConfig(
-        ports = DrivetrainPorts(
-          leftPort = 12,
-          rightPort = 11,
-          leftFollowerPort = 14,
-          rightFollowerPort = 13
-        ),
-        props = DrivetrainProperties(
-          maxLeftVelocity = FeetPerSecond(18.8),
-          maxRightVelocity = FeetPerSecond(19.25),
-          leftVelocityGains = PIDConfig(
-            Ratio(Percent(0), FeetPerSecond(5)),
-            Ratio(Percent(0), Feet(5)),
-            Ratio(Percent(0), FeetPerSecondSquared(5))
-          ),
-          rightVelocityGains = PIDConfig(
-            Ratio(Percent(0), FeetPerSecond(5)),
-            Ratio(Percent(0), Feet(5)),
-            Ratio(Percent(0), FeetPerSecondSquared(5))
-          ),
-          forwardPositionGains = PIDConfig(
-            Percent(0) / Feet(5),
-            Percent(0) / (Feet(5) * Seconds(1)),
-            Percent(0) / FeetPerSecond(5)
-          ),
-          turnVelocityGains = PIDConfig(
-            Percent(0) / DegreesPerSecond(1),
-            Percent(0) / (DegreesPerSecond(1) * Seconds(1)),
-            Percent(0) / (toGenericValue(DegreesPerSecond(1)) / Seconds(1))
-          ),
-          turnPositionGains = PIDConfig(
-            Percent(0) / Degrees(1),
-            Percent(0) / (Degrees(1) * Seconds(1)),
-            Percent(0) / (Degrees(1) / Seconds(1))
-          ),
-          maxTurnVelocity = DegreesPerSecond(90),
-          maxAcceleration = FeetPerSecondSquared(0),
-          defaultLookAheadDistance = Feet(2.5),
-          blendExponent = 0,
-          track = Inches(21.75)
+      driver = Some(
+        DriverConfig(
+          driverPort = 0,
+          operatorPort = 1,
+          driverWheelPort = 2,
+          launchpadPort = -1
         )
-      )),
+      ),
+      drivetrain = Some(
+        DrivetrainConfig(
+          ports = DrivetrainPorts(
+            leftPort = 12,
+            rightPort = 11,
+            leftFollowerPort = 14,
+            rightFollowerPort = 13
+          ),
+          props = DrivetrainProperties(
+            maxLeftVelocity = FeetPerSecond(18.8),
+            maxRightVelocity = FeetPerSecond(19.25),
+            leftVelocityGains = PIDConfig(
+              Ratio(Percent(0), FeetPerSecond(5)),
+              Ratio(Percent(0), Feet(5)),
+              Ratio(Percent(0), FeetPerSecondSquared(5))
+            ),
+            rightVelocityGains = PIDConfig(
+              Ratio(Percent(0), FeetPerSecond(5)),
+              Ratio(Percent(0), Feet(5)),
+              Ratio(Percent(0), FeetPerSecondSquared(5))
+            ),
+            forwardPositionGains = PIDConfig(
+              Percent(0) / Feet(5),
+              Percent(0) / (Feet(5) * Seconds(1)),
+              Percent(0) / FeetPerSecond(5)
+            ),
+            turnVelocityGains = PIDConfig(
+              Percent(0) / DegreesPerSecond(1),
+              Percent(0) / (DegreesPerSecond(1) * Seconds(1)),
+              Percent(0) / (toGenericValue(DegreesPerSecond(1)) / Seconds(1))
+            ),
+            turnPositionGains = PIDConfig(
+              Percent(0) / Degrees(1),
+              Percent(0) / (Degrees(1) * Seconds(1)),
+              Percent(0) / (Degrees(1) / Seconds(1))
+            ),
+            maxTurnVelocity = DegreesPerSecond(90),
+            maxAcceleration = FeetPerSecondSquared(0),
+            defaultLookAheadDistance = Feet(2.5),
+            blendExponent = 0,
+            track = Inches(21.75)
+          )
+        )
+      ),
       forklift = None,
-      cubeLift = Some(CubeLiftConfig(
-        ports = CubeLiftPorts(20),
-        props = CubeLiftProperties(
-          pidConfig = PIDConfig(
-            Percent(0) / Feet(5),
-            Percent(0) / (Feet(5) * Seconds(1)),
-            Percent(0) / FeetPerSecond(5)
-          ),
-          voltageOverHeight = Ratio(Volts(2.5), Inches(42)),
-          talonOverVoltage = Each(1023) / Volts(3.3),
-          voltageAtBottom = Volts(2.94),
-          collectHeight = Inches(10),
-          switchHeight = Inches(20),
-          scaleHeight = Inches(30),
-          switchTolerance = Inches(2),
-          maxMotorOutput = Percent(20),
-          maxHeight = Inches(35),
-          minHeight = Inches(10)
+      cubeLift = Some(
+        CubeLiftConfig(
+          ports = CubeLiftPorts(20),
+          props = CubeLiftProperties(
+            pidConfig = PIDConfig(
+              Percent(0) / Feet(5),
+              Percent(0) / (Feet(5) * Seconds(1)),
+              Percent(0) / FeetPerSecond(5)
+            ),
+            voltageOverHeight = Ratio(Volts(2.5), Inches(42)),
+            talonOverVoltage = Each(1023) / Volts(3.3),
+            voltageAtBottom = Volts(2.94),
+            collectHeight = Inches(10),
+            switchHeight = Inches(20),
+            scaleHeight = Inches(30),
+            switchTolerance = Inches(2),
+            maxMotorOutput = Percent(20),
+            maxHeight = Inches(35),
+            minHeight = Inches(10)
+          )
         )
-      ))
+      )
     ).jencode(RobotConfig.writer).spaces2
   )
 }
