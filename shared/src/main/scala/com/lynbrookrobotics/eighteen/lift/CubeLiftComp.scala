73c0d5aa5abe047f5272e9fc20b38ce59667e98c
Kunal Sheth <kunal@kunalsheth.info>
2018-02-27 09:24:49 -0800

    implement safety incase the cube lift pot is detatched

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftComp.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftComp.scala
@@ -4,13 +4,19 @@ import com.lynbrookrobotics.potassium.Component
 import com.lynbrookrobotics.potassium.control.offload.OffloadedSignal
 import com.lynbrookrobotics.potassium.control.offload.OffloadedSignal.OpenLoop
 import com.lynbrookrobotics.potassium.streams.Stream
-import squants.Each
+import squants.{Each, Percent}
+import squants.electro.Volts
 
 class CubeLiftComp(val coreTicks: Stream[Unit])(implicit hardware: CubeLiftHardware)
     extends Component[OffloadedSignal] {
   override def defaultController: Stream[OffloadedSignal] = coreTicks.mapToConstant(OpenLoop(Each(0)))
 
   override def applySignal(signal: OffloadedSignal): Unit = {
-    hardware.talon.applyCommand(signal)
+    if(hardware.readPotentiometerVoltage() == Volts(0) && !signal.isInstanceOf[OpenLoop]) {
+      hardware.talon.applyCommand(OpenLoop(Percent(0)))
+      println("NO CUBE LIFT POTENTIOMETER DETECTED")
+    } else {
+      hardware.talon.applyCommand(signal)
+    }
   }
 }
