7a34b8073b3f2cafecfd9524d34aae57a3e1b130
Kunal Sheth <kunal@kunalsheth.info>
2018-02-28 05:09:01 -0800

    run scalafmt

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftComp.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/lift/CubeLiftComp.scala
@@ -8,18 +8,19 @@ import squants.electro.Volts
 import squants.{Each, Percent}
 
 class CubeLiftComp(val coreTicks: Stream[Unit])(implicit hardware: CubeLiftHardware)
-  extends Component[OffloadedSignal] {
+    extends Component[OffloadedSignal] {
   override def defaultController: Stream[OffloadedSignal] = coreTicks.mapToConstant(OpenLoop(Each(0)))
 
   override def applySignal(signal: OffloadedSignal): Unit = {
     if (hardware.readPotentiometerVoltage == Volts(0) && !signal.isInstanceOf[OpenLoop]) {
       hardware.talon.applyCommand(OpenLoop(Percent(0)))
       println("[ERROR] No cube lift potentiometer detected")
-    } else signal match {
-      case openLoopSignal: OpenLoop if openLoopSignal.signal.toPercent.abs > 20 =>
-        hardware.talon.applyCommand(OpenLoop(Percent(20) * openLoopSignal.signal.toPercent.signum))
-      case _ =>
-        hardware.talon.applyCommand(signal)
-    }
+    } else
+      signal match {
+        case openLoopSignal: OpenLoop if openLoopSignal.signal.toPercent.abs > 20 =>
+          hardware.talon.applyCommand(OpenLoop(Percent(20) * openLoopSignal.signal.toPercent.signum))
+        case _ =>
+          hardware.talon.applyCommand(signal)
+      }
   }
 }
