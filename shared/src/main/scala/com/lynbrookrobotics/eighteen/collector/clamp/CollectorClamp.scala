08674145abd4477ff504e95367d22d69eb0cfd9e
Kunal Sheth <kunal@kunalsheth.info>
2018-03-01 04:01:00 -0800

    implement SingleOutputChecker and run on clamp component

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/collector/clamp/CollectorClamp.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/collector/clamp/CollectorClamp.scala
@@ -1,21 +1,26 @@
 package com.lynbrookrobotics.eighteen.collector.clamp
 
+import com.lynbrookrobotics.eighteen.SingleOutputChecker
 import com.lynbrookrobotics.potassium.Component
 import com.lynbrookrobotics.potassium.streams.Stream
 
 trait CollectorClampState
 
 case object ClosedClamp extends CollectorClampState
+
 case object OpenClamp extends CollectorClampState
 
 class CollectorClamp(val coreTicks: Stream[Unit])(implicit hardware: CollectorClampHardware)
-    extends Component[CollectorClampState] {
+  extends Component[CollectorClampState] {
   override def defaultController: Stream[CollectorClampState] = coreTicks.mapToConstant(ClosedClamp)
 
-  override def applySignal(signal: CollectorClampState): Unit = {
-    signal match {
-      case ClosedClamp => hardware.solenoid.set(false)
-      case OpenClamp   => hardware.solenoid.set(true)
-    }
-  }
+  val check = new SingleOutputChecker(
+    "Collector Clamp Solenoid",
+    hardware.solenoid.get
+  )
+
+  override def applySignal(signal: CollectorClampState): Unit = check.assertSingleOutput(() => signal match {
+    case ClosedClamp => hardware.solenoid.set(false)
+    case OpenClamp => hardware.solenoid.set(true)
+  })
 }
