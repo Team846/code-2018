6de794d3f1d2fdfd09ce2b4d99888a134e2aeef6
Kunal Sheth <kunal@kunalsheth.info>
2018-03-01 13:18:24 -0800

    make single output checker print expected vs actual values

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/SingleOutputChecker.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/SingleOutputChecker.scala
@@ -5,7 +5,11 @@ class SingleOutputChecker[T](hardwareName: String, get: => T) {
   private var lastOutput: Option[T] = None
 
   def assertSingleOutput(output: () => Unit): Unit = {
-    lastOutput.filter(_ != get).foreach(_ => println(s"[ERROR] $hardwareName has been set twice!"))
+    val currentOutput = get
+    lastOutput.filter(_ != currentOutput).foreach(_ => println(
+      s"""[ERROR] $hardwareName has been set twice!
+         |[ERROR]   -> expected ${lastOutput.get}, but set to $currentOutput""".stripMargin
+    ))
 
     output()
 
