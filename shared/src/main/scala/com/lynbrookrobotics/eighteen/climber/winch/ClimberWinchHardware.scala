670ee82cfdfbf421bba5de261ec0cf2e1b121146
Kunal Sheth <kunal@kunalsheth.info>
2018-03-02 19:15:17 -0800

    enable brake mode on climber winch

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/winch/ClimberWinchHardware.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/climber/winch/ClimberWinchHardware.scala
@@ -1,9 +1,17 @@
 package com.lynbrookrobotics.eighteen.climber.winch
 
+import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.lynbrookrobotics.potassium.frc.LazyTalon
 
-final case class ClimberWinchHardware(leftMotor: LazyTalon, middleMotor: LazyTalon, rightMotor: LazyTalon)
+import scala.collection
+import scala.collection.Set
+
+final case class ClimberWinchHardware(leftMotor: LazyTalon, middleMotor: LazyTalon, rightMotor: LazyTalon) {
+  Set(leftMotor, middleMotor, rightMotor).map(_.t).foreach { it =>
+    it.setNeutralMode(NeutralMode.Brake)
+  }
+}
 
 object ClimberWinchHardware {
   def apply(config: ClimberWinchConfig): ClimberWinchHardware = {
