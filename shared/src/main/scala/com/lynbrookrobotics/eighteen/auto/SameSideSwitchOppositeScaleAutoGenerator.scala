af695f91c82d8622c8949090199e016964895d24
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-04-15 17:36:11 -0700

    Remove unused/untested auto

--- shared/src/main/scala/com/lynbrookrobotics/eighteen/auto/SameSideSwitchOppositeScaleAutoGenerator.scala
+++ shared/src/main/scala/com/lynbrookrobotics/eighteen/auto/SameSideSwitchOppositeScaleAutoGenerator.scala
@@ -1,8 +1,5 @@
 package com.lynbrookrobotics.eighteen.auto
 
-object SameSideSwitchOppositeScalePoints {
-}
-
 trait SameSideSwitchOppositeScaleAutoGenerator extends AutoGenerator with SameSideSwitch {
   object SameSideSwitchOppositeScale {
 
